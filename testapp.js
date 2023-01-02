9761a90ccecd030a1b37e177eaa2e8e2cb4542ae
kliff9 <c.thelusca@hotmail.com>
2023-01-02 03:39:52 -0500

    added 1 line

--- testapp.js
+++ testapp.js
@@ -7,4 +7,6 @@ git checkout -b <branch-name>: Creates a new branch with the specified name and 
 git merge <branch-name>: Merges the specified branch into the current branch.
 git branch -d <branch-name>: Deletes the specified branch.
 git branch -D <branch-name>: Forces the deletion of the specified branch, even if it has unmerged changes.
+git branch -m <new-branch-name>: Renames the current branch to the specified name.
+
 */
