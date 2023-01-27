c1d741c9fdd149fed4df65cff14e6083870dc5b1
kliff9 <c.thelusca@hotmail.com>
2023-01-27 06:33:48 -0500

    1/27/2023

--- pack.js
+++ pack.js
@@ -1,9 +1,11 @@
 /* ------------------- Testing ----------------*/
 // );
 
-const fs = require("fs");
-const zlib = require("zlib");
-const pako = require("pako");
+import fs from "fs";
+import zlib from "zlib";
+import pako from "pako";
+import bodec from "bodec";
+
 /*
 const packfileData = fs.readFileSync(
   ".git/objects/pack/pack-7e55f6d23a104a9f5dcb78ddf187bd9f5da21e44.pack"
@@ -17,16 +19,53 @@ console.log("packfile", packfileData);
 
 */
 
-const packfilePath = '.git/objects/pack/pack-e034723388cd0497ccb7d9baa8581326cec42d44.pack';
+const Desktop_packfilePath =
+  ".git/objects/pack/pack-e034723388cd0497ccb7d9baa8581326cec42d44.pack";
 
-fs.readFile(packfilePath, (err, data) => {
-  if (err) throw err;
-  console.log(data)
+// fs.readFile(packfilePath, (err, data) => {
+//   if (err) throw err;
+//   console.log(data)
 
-  zlib.inflate(data, (err, buffer) => {
-    if (err) throw err;
+//   zlib.inflate(data, (err, buffer) => {
+//     if (err) throw err;
 
-    // buffer contains the decompressed data
-    console.log(buffer);
-  });
-});
+//     // buffer contains the decompressed data
+//     console.log(buffer);
+//   });
+// });
+
+// Assume filePath is the path to the .pack file
+const Laptop_filePath =
+  ".git/objects/pack/pack-7e55f6d23a104a9f5dcb78ddf187bd9f5da21e44.pack";
+
+const indexFile =
+  ".git/objects/pack/pack-7e55f6d23a104a9f5dcb78ddf187bd9f5da21e44.idx";
+
+// Read the index file
+const indexData = fs.readFileSync(indexFile);
+
+// Read the binary data of the .pack file
+console.log(fs.statSync(Laptop_filePath).isFile());
+// const packread = fs.read(Laptop_filePath);
+
+const packData = fs.readFileSync(Laptop_filePath);
+
+// Unzip the data using zlib
+
+console.log(indexData);
+console.log(packData);
+let buffer = packData.toString();
+console.log(buffer);
+
+// const unzippedData = zlib.gunzipSync(packData);
+// const unzippedData = zlib.unzipSync(packData);
+// const unzippedData = zlib.inflateSync(packData);
+
+// Extract the files from the unzipped data
+// The format of the unzipped data is specific to the git packfile format
+// and may require additional parsing and processing to extract the files
+
+// const decompressedData = zlib.inflateSync(packData);
+
+const hexString = bodec.toHex(packData);
+// console.log(hexString); // "0102030405"
