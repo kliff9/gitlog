325d4d2ab821ae91da944a645326f25c6928c481
kliff <c.thelusca@hotmail.com>
2023-02-03 14:08:30 -0500

    2/23

--- p.js
+++ p.js
@@ -6,24 +6,34 @@ import bodec from "bodec";
 const Laptop_filePath =
   ".git/objects/pack/pack-7e55f6d23a104a9f5dcb78ddf187bd9f5da21e44.pack";
 
-// let packData = fs.readFileSync(Laptop_filePath);
+const Desktop_packfilePath =
+  ".git/objects/pack/pack-e034723388cd0497ccb7d9baa8581326cec42d44.pack";
 
-const packfile = fs.readFileSync(Laptop_filePath);
 
-const header = packfile.slice(0, 12);
-const numObjects = packfile.readUInt32BE(8);
+// const packfile = fs.readFileSync(Laptop_filePath);
 
-fs.readFile(Laptop_filePath, (err, data) => {
+fs.readFile(Desktop_packfilePath, (err, data) => {
   if (err) throw err;
 
-  const header = data.slice(0, 12);
-  if (header[0] !== 0x78 || header[1] !== 0x9c) {
-    console.error("Data is not in zlib format");
-  }
-  const _32 = data.slice(32, 44);
-  console.log(data);
+  // First object in a pack file starts at byte 12
+  const firstObject = data.slice(12);
 
-  // continue with decompression
-  const decompressedData = pako.inflate(data);
-  console.log(decompressedData);
+  // Do something with the first object data
+  console.log(firstObject);
+  zlib.inflate(data, (err, inflatedData) => {
+    if (err) {
+      console.error("Could not inflate data: ", err);
+      return;
+    }
+    console.log(inflatedData)
+
+    // Incorrect error check:
+    // Check for error even if there is no error
+    if (err) {
+      console.error("Error occurred: ", err);
+      return;
+    }
+
+    // Parse the first object from the inflated data here...
+  });
 });
