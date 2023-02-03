6e47f151423f7774ff1d4c00cd2da20fca7ba34b
kliff9 <c.thelusca@hotmail.com>
2023-02-03 06:42:16 -0500

    2/3/ changes

--- GLL.js
+++ GLL.js
@@ -1,45 +1,48 @@
 // const fs = require("fs");
 // const zlib = require("zlib");
 // const parse = require("./parse.js");
-import fs from 'fs'
-import zlib from 'zlib'
-import parse from './parse.js';
-import path from 'path';
+import fs from "fs";
+import zlib from "zlib";
+import parse from "./parse.js";
+import path from "path";
 
 const folder_name = process.argv[2];
 console.log(folder_name);
 
-let folder_path
+let folder_path;
 const current_folder_path = process.cwd();
 let parent_folder_path = path.dirname(current_folder_path);
 // folder_path = path.join(parent_folder_path, folder_name);
-folder_path = folder_name
-console.log(folder_path);
-
+folder_path = folder_name;
+console.log(folder_path); // C:\Users\Kliff_\Desktop\LESSONS\digital-garden
 
 // let testopen = fs.readFileSync("C:/Users/cthel/OneDrive/Desktop/LESSONS/Git/digital-garden/.git/refs/heads/master", "utf8");
 // console.log('testopen: ', testopen)
 
-const ref_ = (ref, path) => `${path}/.git/refs/${ref}/master`;
+const ref_ = (ref, path) => `${path}/.git/${ref}`;
 
-let mailmap = parse()
-// console.log(mailmap) 
+let mailmap = parse(folder_path);
+// console.log(mailmap)
 
 function check_mailmap(author_email) {
- let result = false
+  let result = false;
   // check if key(email) is in author line
   if (Object.keys(mailmap).includes(author_email)) {
-    result = mailmap[author_email]
+    result = mailmap[author_email];
   }
-  return result
+  return result;
 }
 
 function git_log() {
-  let head = "heads";
-  let branch_recent_commit = fs.readFileSync(ref_(head, folder_path), "utf8");
-console.log(branch_recent_commit)
+  const headFile = fs.readFileSync(`${folder_path}/.git/HEAD`, "utf8");
+  const branchName = headFile.split(":").pop().trim();
+  console.log(branchName);
+  let branch_recent_commit = fs.readFileSync(
+    ref_(branchName, folder_path),
+    "utf8"
+  );
+  console.log(branch_recent_commit);
   for (let i = 0; i < 3; i++) {
-
     const CommitStart = branch_recent_commit.substring(0, 2); // fisrt two characters of the commit hash
     const Commitfilled = branch_recent_commit.substring(2); // the rest of characters of the commit hash
     let commit_data_path = `${folder_path}/.git/objects/${CommitStart}/${Commitfilled}`; // path to the commit data
@@ -53,61 +56,54 @@ console.log(branch_recent_commit)
     const InfoLine = commitLines.find((line) => line.startsWith("author"));
     const parent_line = commitLines.find((line) => line.startsWith("parent"));
 
-      const parent = parent_line.split(" ")[1];
-      let Author_name = InfoLine.split(" ")[1];
-      let author_email = InfoLine.split(" ")[2];
-      // console.log('PREE',Author_name, author_email)
+    const parent = parent_line.split(" ")[1];
+    let Author_name = InfoLine.split(" ")[1];
+    let author_email = InfoLine.split(" ")[2];
+    // console.log('PREE',Author_name, author_email)
 
-      if (mailmap[author_email]){ 
-        let modified_Author = check_mailmap(author_email)
+    if (mailmap[author_email]) {
+      let modified_Author = check_mailmap(author_email);
 
-        if(modified_Author && modified_Author.split(" ").includes("filler")) {
-          if (!modified_Author.split(" ")[0].includes('filler')) {
-            Author_name = modified_Author.split(/( <.*?>)/)[0]
-
-          }
-          if (!modified_Author.split(" ")[1].includes('<filler')) { //includes so dont change
-            console.log('true')
-            author_email = modified_Author.split(/( <.*?>)/)[1];
-
-          } 
-        } else if (modified_Author === false ) {
-
-          console.log("failed")
+      if (modified_Author && modified_Author.split(" ").includes("filler")) {
+        if (!modified_Author.split(" ")[0].includes("filler")) {
+          Author_name = modified_Author.split(/( <.*?>)/)[0];
+        }
+        if (!modified_Author.split(" ")[1].includes("<filler")) {
+          //includes so dont change
+          console.log("true");
+          author_email = modified_Author.split(/( <.*?>)/)[1];
+        }
+      } else if (modified_Author === false) {
+        console.log("failed");
       } else {
-        Author_name = modified_Author.split(/( <.*?>)/)[0]
+        Author_name = modified_Author.split(/( <.*?>)/)[0];
         author_email = modified_Author.split(/( <.*?>)/)[1];
       }
-          
-      } 
-
-      console.log(`commit: ${branch_recent_commit}`);
-
-      console.log(`Arthor: ${Author_name} ${author_email}`);
-
-
-      const Date_ = InfoLine.split(">")[1];
-
-      const timestamp = Date_.split(" ")[1];
-      let date = new Date(timestamp * 1000);
-      let options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      };
-      let formattedDate = date.toLocaleString("en-US", options);
-
-      console.log(`Date: ${formattedDate}\n`);
-
-      branch_recent_commit = parent;
     }
-  
+
+    console.log(`commit: ${branch_recent_commit}`);
+
+    console.log(`Arthor: ${Author_name} ${author_email}`);
+
+    const Date_ = InfoLine.split(">")[1];
+
+    const timestamp = Date_.split(" ")[1];
+    let date = new Date(timestamp * 1000);
+    let options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    let formattedDate = date.toLocaleString("en-US", options);
+
+    console.log(`Date: ${formattedDate}\n`);
+
+    branch_recent_commit = parent;
+  }
 }
 
 git_log();
-
-
--- parse.js
+++ parse.js
@@ -1,80 +1,84 @@
-import fs from 'fs'
+import fs from "fs";
+import path from "path";
 
-export default function parse() {
-  const file = fs.readFileSync(".mailmap", "utf8");
+export default function parse(file_path) {
+  let file;
+
+  try {
+    let folder_path = path.join(file_path, "/.mailmap");
+    file = fs.readFileSync(folder_path, "utf8");
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 
   const array1 = file.trim().split(/\n/); // mailmap split line by line
   let mailmap = {};
-  let email = '<filler__email>' // filler value for email for Proper Name <commit@email.xx>
-  let username = 'filler__name'// filler value for name for <proper@email.xx> <commit@email.xx>
+  let email = "<filler__email>"; // filler value for email for Proper Name <commit@email.xx>
+  let username = "filler__name"; // filler value for name for <proper@email.xx> <commit@email.xx>
 
   let array_length = array1.length; // for each line in mailmap
   for (let i = 0; i < array_length; i++) {
-
-    let current_array = array1[i].split(/( <.*?>)/) // Ex [ 'User Stickas', '<Jamacian@hotmail@.xx>', 'Kliff124', '<121292926+kliff124@users.noreply.github.com>' ]
-    .map(elem => elem.trim())                       // Grab each word in the string in translate to array string
-    .filter(elem => elem !== ""); //
+    let current_array = array1[i]
+      .split(/( <.*?>)/) // Ex [ 'User Stickas', '<Jamacian@hotmail@.xx>', 'Kliff124', '<121292926+kliff124@users.noreply.github.com>' ]
+      .map((elem) => elem.trim()) // Grab each word in the string in translate to array string
+      .filter((elem) => elem !== ""); //
     // console.log(current_array)
 
-    if (current_array.length === 3) { // Format: Proper Name <proper@email.xx> <commit@email.xx>
-      email = current_array[0]
-      username = current_array[1]
-      mailmap[current_array[2]] = email + " "+ username;
-    } else if (current_array.length === 4) { // Proper Name <proper@email.xx> Commit Name <commit@email.xx>
-      email = current_array[0]
-      username = current_array[1]
-      mailmap[current_array[3]] = email + " "+ username;
-    } else if (current_array.length === 2) { // format <proper@email.xx> <commit@email.xx> / Proper Name <commit@email.xx>
-        if (mailmap[current_array[1]]) {
-           if (current_array[0].includes("@")) {
+    if (current_array.length === 3) {
+      // Format: Proper Name <proper@email.xx> <commit@email.xx>
+      email = current_array[0];
+      username = current_array[1];
+      mailmap[current_array[2]] = email + " " + username;
+    } else if (current_array.length === 4) {
+      // Proper Name <proper@email.xx> Commit Name <commit@email.xx>
+      email = current_array[0];
+      username = current_array[1];
+      mailmap[current_array[3]] = email + " " + username;
+    } else if (current_array.length === 2) {
+      // format <proper@email.xx> <commit@email.xx> / Proper Name <commit@email.xx>
+      if (mailmap[current_array[1]]) {
+        if (current_array[0].includes("@")) {
+          // Split the value associated with the key
+          let value = mailmap[current_array[1]].split(/( <.*?>)/);
 
-       // Split the value associated with the key 
-          let value = mailmap[current_array[1]].split(/( <.*?>)/)
+          // Update the 2nd word
+          value[1] = current_array[0];
 
-        // Update the 2nd word
-        value[1] = current_array[0]
+          // Join the array of words back into a single string
+          let updated_value = value.join("");
 
-        // Join the array of words back into a single string
-        let updated_value = value.join("")
+          // Update the value in the mailmap object
+          mailmap[current_array[1]] = updated_value;
+        } else {
+          let value2 = mailmap[current_array[1]].split(/( <.*?>)/);
 
-        // Update the value in the mailmap object
-        mailmap[current_array[1]] = updated_value
+          // Update the 2nd word
+          value2[0] = current_array[0];
 
-           } else {
-            let value2 = mailmap[current_array[1]].split(/( <.*?>)/)
+          // Join the array of words back into a single string
+          let updated_value2 = value2.join("");
 
-            // Update the 2nd word
-                    value2[0] = current_array[0]
-            
-            // Join the array of words back into a single string
-                    let updated_value2 = value2.join("")
-            
-            // Update the value in the mailmap object
-                    mailmap[current_array[1]] = updated_value2
-           }
-          } else {
-          if (current_array[0].includes("@")) { // Intialize for Single Changes Ex. 
-            email = current_array[0]
-            mailmap[current_array[1]] = 'filler ' +  email   // '<c.thelusca@hotmail.com>': 'filler <mailmoo@gmail.com>',
-
-          } else {
-            username = current_array[0]
-            mailmap[current_array[1]] = username +  "<filler>" //// '<c.thelusca@hotmail.com>': 'Khun filler',
-          }
-
+          // Update the value in the mailmap object
+          mailmap[current_array[1]] = updated_value2;
         }
-
+      } else {
+        if (current_array[0].includes("@")) {
+          // Intialize for Single Changes Ex.
+          email = current_array[0];
+          mailmap[current_array[1]] = "filler " + email; // '<c.thelusca@hotmail.com>': 'filler <mailmoo@gmail.com>',
+        } else {
+          username = current_array[0];
+          mailmap[current_array[1]] = username + " <filler>"; //// '<c.thelusca@hotmail.com>': 'Khun filler',
+        }
+      }
     } else {
       console.log("error");
     }
   }
+  console.log(mailmap);
+
   return mailmap;
-
 }
-// console.log(parse())
 
-
-
-
-  // let emailRegex = /<[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}>/;
-
+// let emailRegex = /<[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}>/;
